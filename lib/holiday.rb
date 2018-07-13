c284df117ce17e025dd417595221edf5599b564f
Richard Ojo <richardojo04@gmail.com>
2018-07-13 15:51:07 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,32 +18,38 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter][:christmas] << supply
+holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply
 end
 
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+def add_new_holiday_with_supplies(hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+#hash[season] = {}
+hash[season][holiday_name] = supply_array
+#hash[season][holiday_name] << supply_array
 
+hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
-end
+  holiday_hash[:winter].values.flatten 
+  end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -53,15 +59,23 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do | parent_key, parent_value|
+   puts "#{parent_key.capitalize}:"
+   parent_value.each do |child_key, child_value|
+     puts "  #{child_key.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{child_value.join(", ")}"
+   end
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.map do |parent_key, parent_value|
+    parent_value.map do |child_key, child_value|
+      child_key if child_value.include?("BBQ")
+    end 
+  end.flatten.compact
 end
-
 
 
 
